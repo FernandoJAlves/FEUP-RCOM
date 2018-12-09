1fbb77e29aebbcfa5288c9bd63d3cc2e1072d9af
FernandoJAlves <up201605270@fe.up.pt>
2018-12-09 17:23:58 +0000

    Incorporated more of the provided code

--- Proj2/Download Application/downloadApplication.c
+++ Proj2/Download Application/downloadApplication.c
@@ -9,8 +9,12 @@
 #include <sys/types.h>
 #include <netinet/in.h> 
 #include <arpa/inet.h>
+#include <unistd.h>
+#include <signal.h>
+#include <strings.h>
 
 #define MAX_STRING_SIZE     128
+#define SERVER_PORT 		21
 
 // Example of input: ./run ftp://[<user>:<password>@]<host>/<url-path>
 void parseInputString(char *input, char *user, char *password, char *host, char *url_path){
@@ -110,26 +114,22 @@ void parseFile(char *url_path, int url_path_size, char *filename){
 	}
 }
 
-
-
-
-
 // Codigo fornecido
 
-
-void getip(char* host, struct hostent *h)
+struct hostent* getip(char* host)
 {
-    if ((h = gethostbyname(host)) == NULL) {  
+    struct hostent * h;
+	if ((h = gethostbyname(host)) == NULL) {  
         herror("gethostbyname");
         exit(1);
     }
-
-    printf("Host name  : %s\n", h->h_name);
-    printf("IP Address : %s\n\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
+	return h;
 }
 
-
 // Fim de Codigo fornecido
+
+
+
 
 
 int main(int argc, char** argv){
@@ -160,12 +160,39 @@ int main(int argc, char** argv){
     printf("URL Path: %s\n", url_path);
     printf("File name: %s\n\n", filename);
 
-    struct hostent * h;
+    struct hostent* h = getip(host);
 
-    getip(host, h);
+    printf("Host name  : %s\n", h->h_name);
+    printf("IP Address : %s\n\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
 
+	// Codigo fornecido 
 
+	int	sockfd;
+	struct	sockaddr_in server_addr;
+	char buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
+	int	bytes;
 
+	/*server address handling*/
+	bzero((char*)&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)h->h_addr)));	/*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
+    
+	/*open an TCP socket*/
+	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+    	perror("socket()");
+        exit(0);
+    }
+
+	/*connect to the server*/
+    if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+        perror("connect()");
+		exit(0);
+	}
+
+    /*send a string to the server*/
+	bytes = write(sockfd, buf, strlen(buf));
+	printf("Bytes escritos %d\n", bytes);
 
 
 
